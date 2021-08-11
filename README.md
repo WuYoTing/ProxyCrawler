31c28c0211d9f4a71bb711ce7c42336d953cedbd
WuYoTing <hakosaki314@gmail.com>
2021-08-11 13:46:55 +0800

    feat: 更新READ.ME 及 調整驗證proxy 可用性的方法

--- README.md
+++ README.md
@@ -5,4 +5,12 @@ a simple crawler to crawl proxy
 
 Todo List
 - [ ] 新增更多網站
+- [Free Proxy](http://free-proxy.cz/en/)
+- [ProxyScrape](https://proxyscrape.com/free-proxy-list)
+- [proxynova](https://www.proxynova.com/proxy-server-list)
+- [SSL Proxy](https://www.sslproxies.org/)
+- [Hide.me](https://hide.me/en/proxy)
+- [HMA](https://www.hidemyass.com/en-us/proxy)
+- [Open Proxy Space](https://openproxy.space/list)
+- [ProxyScan ](https://www.proxyscan.io/)
 - [ ] 處理reCAPTCHA驗證問題
--- webside/free_proxy_cz.py
+++ webside/free_proxy_cz.py
@@ -8,8 +8,7 @@ def free_proxy_cz(driver):
     for page in range(1, 11):
         print("{} : {}".format("page", page))
         # 用迴圈逐一打開分頁
-        url = 'http://free-proxy.cz/en/proxylist/main/ping/{}'.format(page)
-        print('Dealing with {}'.format(url))
+        url = 'http://free-proxy.cz/en/proxylist/country/all/http/ping/all/{}'.format(page)
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'lxml')
         for tbody_tr in soup.select('tbody > tr'):
@@ -18,16 +17,16 @@ def free_proxy_cz(driver):
                 IP = re.findall('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', str(tbody_tr))[0]
                 Port = re.findall('class="fport" style="">(.*?)</span>', str(tbody_tr))[0]
                 # check proxy effectiveness
-                proxy = {'http': 'http://' + IP + ':' + Port,
-                         'https': 'https://' + IP + ':' + Port}
+                proxy = {'http': 'http://' + IP + ':' + Port}
                 try:
-                    url = 'https://www.chinatimes.com/realtimenews/20200205004069-260408'
-                    resp = requests.get(url, proxies=proxy, timeout=2)
-                    if str(resp.status_code) == '200':
+                    url = 'http://icanhazip.com/'
+                    resp = requests.get(url, proxies=proxy, timeout=8)
+                    proxy_ip = resp.text
+                    if proxy_ip == IP:
                         ip_pool[IP] = Port
                         print('Succed: {}:{}'.format(IP, Port))
                     else:
                         print('Failed: {}:{}'.format(IP, Port))
-                except:
-                    print('Failed: {}:{}'.format(IP, Port))
+                except Exception as e:
+                    print('Except , Failed: {}:{} , e: {}'.format(IP, Port, e))
     print('There are {} IPs in Pool'.format(len(ip_pool)))
